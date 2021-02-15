a188652a29782ae693ac5d9995c17a5c1b4e6b02
madiro98 <74104738+madiro98@users.noreply.github.com>
2021-02-15 17:22:06 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='Macromolecular PPIs',
+        version='1.0',
+        description='Description of my project',
+        author='Aleix Canalda Baltrons & Maria Díaz Ros',
+        author_email=['aleix.canalda01@estudiant.upf.edu', 'maria.diaz07@estudiant.upf.edu'],
+        packages=['', ''],
+        install_requires=['Biopython', '']
+        )
