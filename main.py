91c5ee818daed8e36da06b24e02f692688332143
aleixcanalda <55092745+aleixcanalda@users.noreply.github.com>
2021-03-29 11:49:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from IOinterface import *
 from classes import *
-from build_complex import *
+from build_complex4 import *
 from energies import *
 
 prots_files = get_input_file(options.input_directory)
@@ -11,7 +11,10 @@ chain_list, nomen = all_chains(prots_files, options.verbose)
 
 int_dict = get_interactions_dict(chain_list, prots_files, options.verbose, options.template)
 
-model = main_loop(chain_list, int_dict, options.verbose, options. stechiometry, options.template)
+if options.template is not None:
+	model = template_loop(chain_list, int_dict, nomen, options.template, options.verbose, options. stechiometry)
+else:
+	model = main_loop(chain_list, int_dict, nomen, options.verbose, options. stechiometry)
 
 filename = save_PDB(model, options.output_directory, options.verbose)
 
