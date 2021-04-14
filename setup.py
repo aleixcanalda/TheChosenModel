7b44316afb88a8b4589ccbab781f31be8c6ccc7d
aleixcanalda <55092745+aleixcanalda@users.noreply.github.com>
2021-04-14 14:25:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
-setup(name='Macromolecular PPIs',
+setup(name='TheChosenModel',
         version='1.0',
         description='Description of my project',
         author='Aleix Canalda Baltrons & Maria Díaz Ros',
         author_email=['aleix.canalda01@estudiant.upf.edu', 'maria.diaz07@estudiant.upf.edu'],
         packages=['', ''],
         install_requires=['Biopython']
+        scripts=['scripts/TheChosenModel.py']
         )
