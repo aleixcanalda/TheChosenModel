3e840bddd16f2cda6821721c5214d71f30974433
madiro98 <74104738+madiro98@users.noreply.github.com>
2021-04-15 17:56:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,14 @@ setup(name='TheChosenModel',
         description='Description of my project',
         author='Aleix Canalda Baltrons & Maria Díaz Ros',
         author_email=['aleix.canalda01@estudiant.upf.edu', 'maria.diaz07@estudiant.upf.edu'],
-        install_requires=['biopython>=1.73.0'],
-        packages=['scripts']
+        url='https://github.com/aleixcanalda/TheChosenModel',
+        install_requires=['biopython>=1.73'],
+        license='LICENSE.txt',
+        classifiers=[
+                "Programming Language :: Python :: 3",
+	              "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent"],
+        packages=['scripts'],
+        scripts=["scripts/TheChosenModel.py", "scripts/IOinterface.py", "scripts/classes.py",
+                "scripts/build_complex4.py", "scripts/energies.py"]
         )
