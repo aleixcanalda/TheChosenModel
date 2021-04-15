7b52b09f5b9d7d0d780bb64420b62bc22e385539
aleixcanalda <55092745+aleixcanalda@users.noreply.github.com>
2021-04-15 13:18:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='TheChosenModel',
         description='Description of my project',
         author='Aleix Canalda Baltrons & Maria Díaz Ros',
         author_email=['aleix.canalda01@estudiant.upf.edu', 'maria.diaz07@estudiant.upf.edu'],
-        install_requires=['biopython>=1.73'],
+        install_requires=['biopython>=1.73.0'],
         packages=['scripts']
         )
